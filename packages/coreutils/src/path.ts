53fa75c31f021251b9d0bf4d0c2c1e953c02f7c6
Luciano Resende <lresende@apache.org>
2020-10-31 12:18:40 -0700

    Switch import from path-posix to path-browserify

--- packages/coreutils/src/path.ts
+++ packages/coreutils/src/path.ts
@@ -1,7 +1,7 @@
 // Copyright (c) Jupyter Development Team.
 // Distributed under the terms of the Modified BSD License.
 
-import * as posix from 'path-posix';
+import * as posix from 'path-browserify';
 
 /**
  * The namespace for path-related functions.
